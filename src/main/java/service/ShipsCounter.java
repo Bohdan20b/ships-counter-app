9515e1d115d7e31d5bceb555355aacded4e0d86c
Bohdan Senyk <senyk2001@gmail.com>
2023-03-02 18:55:36 +0200

    reworked logic according to task

--- src/main/java/service/ShipsCounter.java
+++ src/main/java/service/ShipsCounter.java
@@ -4,26 +4,26 @@ public class ShipsCounter {
 
     public static int countShips(int[][] field) {
         int shipCount = 0;
-        int shipLength = 0;
-        for (int[] ints : field) {
-            for (int i : ints) {
-                switch (i) {
-                    case 1:
-                        if (shipLength == 0) {
-                            shipCount++;
-                        }
-                        shipLength++;
-                        continue;
-                    case 0:
-                        if (shipLength > 0) {
-                            shipLength = 0;
-                        }
-                        continue;
-                    default:
-                        throw new RuntimeException("Wrong input type. It should be either 0 or 1.");
+        int[][] testField = new int[field.length + 1][field[0].length + 1];
+
+        for (int i = 0; i < testField.length - 1; i++) {
+            for (int j = 0; j < testField[0].length - 1; j++) {
+                testField[i][j] = field[i][j];
+            }
+        }
+
+        for (int i = testField.length - 1; i < testField.length; i++) {
+            for (int j = testField[0].length - 1; j < testField.length; j++) {
+                testField[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < testField.length; i++) {
+            for (int j = 0; j < testField[0].length; j++) {
+                if (testField[i][j] == 1 && testField[i + 1][j] == 0 && testField[i][j + 1] == 0) {
+                    shipCount++;
                 }
             }
-            shipLength = 0;
         }
         return shipCount;
     }
